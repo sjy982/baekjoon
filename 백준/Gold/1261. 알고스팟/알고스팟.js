2e94c8ba68f5f1e4d9f3b8cd4f981dd74f0dd799
sarnod <89844585+sjy982@users.noreply.github.com>
2022-10-13 16:57:24 +0900

    Update 알고스팟.js

--- 백준/Gold/1261. 알고스팟/알고스팟.js
+++ 백준/Gold/1261. 알고스팟/알고스팟.js
@@ -1,3 +1,4 @@
+//0-1 BFS 
 class Deque {
     constructor() {
         this.storage = {},
@@ -69,6 +70,79 @@ function BFS() {
                     } else {
                         visited[ny][nx] = visited[y][x] + 1;
                         deque.push_back([nx, ny]);
+                    }
+                }
+            }
+        }
+    }
+}
+
+//다익스트라 알고리즘 풀이 --------
+class Queue {
+    constructor() {
+        this.storage = {},
+        this.front = 0;
+        this.rear = -1;
+    }
+    
+    size() {
+        if(this.front > this.rear) {
+            return 0;
+        } else {
+            return this.rear - this.front + 1;
+        }
+    }
+    
+    push(value) {
+        this.rear +=1;
+        this.storage[this.rear] = value;
+    }
+    
+    pop_left() {
+        let value = this.storage[this.front];
+        if(this.size()) {
+            delete this.storage[this.front];
+            this.front += 1;
+        }
+        return value;
+    }
+}
+const fs = require('fs');
+let inputData = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let [N,M] = inputData[0].trim().split(' ').map(x=>x*1);
+let graph = Array.from(Array(M), () => Array());
+for(let i=1; i<inputData.length; i++) {
+    let input = inputData[i].trim().split('').map(x=>x*1);
+    for(let j=0; j<input.length; j++) {
+        graph[i-1][j] = input[j];
+    }
+}
+let visited = Array.from(Array(M), () => Array(N).fill(Infinity));
+BFS();
+console.log(visited[M-1][N-1]);
+
+function BFS() {
+    //다익스트라 알고리즘 사용 => queue를 이용
+    let que = new Queue();
+    que.push([0,0]);
+    visited[0][0] = 0;
+    let vx = [0,1,0,-1];
+    let vy = [-1,0,1,0];
+    while(que.size() !== 0) {
+        let [x,y] = que.pop_left();
+        for(let i=0; i<4; i++) {
+            let nx = x + vx[i];
+            let ny = y + vy[i];
+            if((nx>=0 && nx<=N-1) && (ny>=0 && ny<=M-1)) {
+                if(graph[ny][nx] === 0) {
+                    if(visited[ny][nx] > visited[y][x]) {
+                        visited[ny][nx] = visited[y][x];
+                        que.push([nx,ny]);
+                    }
+                } else {
+                    if(visited[ny][nx] > visited[y][x] + 1) {
+                        visited[ny][nx] = visited[y][x] + 1;
+                        que.push([nx,ny]);
                     }
                 }
             }
