f3b527778e54cf43d2be1f6dcfd0e085ee30ee20
Sarnold <89844585+sjy982@users.noreply.github.com>
2022-11-12 16:14:23 +0900

    [Platinum III] Title: 롤러코스터, Time: 600 ms, Memory: 88348 KB -BaekjoonHub

--- 백준/Platinum/2873. 롤러코스터/롤러코스터.java
+++ 백준/Platinum/2873. 롤러코스터/롤러코스터.java
@@ -2,212 +2,163 @@ import java.io.*;
 import java.util.*;
 
 class Coordinate {
-    int x, y;
+    int x,y;
     Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
 
-class CoordinateInfo {
-    int x, y;
-    char d;
-    CoordinateInfo(int x, int y, char d) {
-        this.x = x;
-        this.y = y;
-        this.d = d;
-    }
-}
-
 public class Main {
+    static int R,C;
+    static Coordinate min = new Coordinate(-1,-1);
     static int map[][];
-    static int R, C;
-    static char d;
     static StringBuilder ans = new StringBuilder();
-    static Coordinate min_coordinate = new Coordinate(-1, -1);
-    static CoordinateInfo cur;
-    static char pattern_d;
-    static char next_d;
-    static int last_x;
-    static int wx;
+    static boolean last_ex = false;
     public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        map = new int[R][C];
-        for (int i = 0; i < R; i++) {
-            StringTokenizer sti = new StringTokenizer(br.readLine());
-            for (int j = 0; j < C; j++) {
-                map[i][j] = Integer.parseInt(sti.nextToken());
-                if (((i % 2 == 1) && (j % 2 == 0)) || ((i % 2 == 0) && (j % 2 == 1))) {
-                    if (min_coordinate.x == -1) {
-                        min_coordinate = new Coordinate(j, i);
-                    } else {
-                        if (map[min_coordinate.y][min_coordinate.x] > map[i][j]) {
-                            min_coordinate = new Coordinate(j, i);
-                        }
-                    }
-                }
-            }
-        }
-        if (R % 2 == 1) {
-            cur = new CoordinateInfo(0, 0, 'R');
-            while (cur.x != C - 1 || cur.y != R - 1) {
-                if (cur.d == 'R') {
-                    if (cur.x == C - 1) {
-                        ans.append("D");
-                        cur = new CoordinateInfo(cur.x, cur.y + 1, 'L');
-                    } else {
-                        ans.append("R");
-                        cur = new CoordinateInfo(cur.x + 1, cur.y, 'R');
-                    }
-                } else if (cur.d == 'L') {
-                    if (cur.x == 0) {
-                        ans.append("D");
-                        cur = new CoordinateInfo(cur.x, cur.y + 1, 'R');
-                    } else {
-                        ans.append("L");
-                        cur = new CoordinateInfo(cur.x - 1, cur.y, 'L');
-                    }
-                }
-            }
-        } else if (C % 2 == 1) {
-            cur = new CoordinateInfo(0, 0, 'D');
-            while (cur.x != C - 1 || cur.y != R - 1) {
-                if (cur.d == 'D') {
-                    if (cur.y == R - 1) {
-                        ans.append("R");
-                        cur = new CoordinateInfo(cur.x + 1, cur.y, 'U');
-                    } else {
-                        ans.append("D");
-                        cur = new CoordinateInfo(cur.x, cur.y + 1, 'D');
-                    }
-                } else if (cur.d == 'U') {
-                    if (cur.y == 0) {
-                        ans.append("R");
-                        cur = new CoordinateInfo(cur.x + 1, cur.y, 'D');
-                    } else {
-                        ans.append("U");
-                        cur = new CoordinateInfo(cur.x, cur.y - 1, 'U');
-                    }
-                }
-            }
-        } else {
-            cur = new CoordinateInfo(0, 0, 'R');
-            if (min_coordinate.y == R - 1) {
-                //마지막 줄에 칸이 제외 됐다면
-                while (cur.x != C - 1 || cur.y != R - 1) {
-                    if (cur.y + 1 == min_coordinate.y) {
-                        if (cur.d == 'R') {
-                            if (cur.x == min_coordinate.x) {
-                                ans.append("R");
-                                cur = new CoordinateInfo(cur.x + 1, cur.y, 'R');
-                            } else {
-                                ans.append("D");
-                                cur = new CoordinateInfo(cur.x, cur.y + 1, 'D');
-                            }
-                        } else if (cur.d == 'U') {
-                            ans.append("R");
-                            cur = new CoordinateInfo(cur.x + 1, cur.y, 'R');
-                        }
-                    } else if (cur.y == min_coordinate.y) {
-                        if (cur.d == 'R') {
-                            ans.append("U");
-                            cur = new CoordinateInfo(cur.x, cur.y - 1, 'U');
-                        } else if (cur.d == 'D') {
-                            ans.append("R");
-                            cur = new CoordinateInfo(cur.x + 1, cur.y, 'R');
-                        }
-                    } else {
-                        if (cur.d == 'R') {
-                            if (cur.x == C - 1) {
-                                ans.append("D");
-                                cur = new CoordinateInfo(cur.x, cur.y + 1, 'L');
-                            } else {
-                                ans.append("R");
-                                cur = new CoordinateInfo(cur.x + 1, cur.y, 'R');
-                            }
-                        } else if (cur.d == 'L') {
-                            if (cur.x == 0) {
-                                ans.append("D");
-                                cur = new CoordinateInfo(cur.x, cur.y + 1, 'R');
-                            } else {
-                                ans.append("L");
-                                cur = new CoordinateInfo(cur.x - 1, cur.y, 'L');
-                            }
-                        }
-                    }
-                }
-
-            } else {
-                if(min_coordinate.y % 2 == 0) {
-                    pattern_d = 'R';
-                    next_d = 'L';
-                    wx = 1;
-                    last_x = C-1;
-                } else {
-                    pattern_d = 'L';
-                    next_d = 'R';
-                    wx = -1;
-                    last_x = 0;
-                }
-                while (cur.x != C - 1 || cur.y != R - 1) {
-                    if(cur.y == min_coordinate.y) {
-                        if(cur.d == pattern_d) {
-                            ans.append("D");
-                            cur = new CoordinateInfo(cur.x,cur.y+1,'D');
-                        } else if(cur.d == 'U') {
-                            ans.append(pattern_d);
-                            cur = new CoordinateInfo(cur.x + wx, cur.y, pattern_d);
-                        }
-                        
-                    } else if(cur.y-1 == min_coordinate.y) {
-                        if(cur.d == pattern_d) {
-                            if(cur.x == min_coordinate.x) {
-                                if(cur.x == last_x) {
-                                    ans.append("D");
-                                    cur = new CoordinateInfo(cur.x, cur.y+1, next_d);
-                                } else {
-                                    ans.append(pattern_d);
-                                    cur = new CoordinateInfo(cur.x + wx, cur.y, pattern_d);
-                                }
-                            } else {
-                                ans.append("U");
-                                cur = new CoordinateInfo(cur.x, cur.y-1, 'U');
-                            }
-                        } else {
-                            if(cur.x == last_x) {
-                                ans.append("D");
-                                cur = new CoordinateInfo(cur.x, cur.y+1, next_d);
-                            } else {
-                                ans.append(pattern_d);
-                                cur = new CoordinateInfo(cur.x + wx, cur.y, pattern_d);
-                            }
-                        }
-                    } else {
-                        if(cur.d == 'R') {
-                            if(cur.x == C - 1) {
-                                ans.append("D");
-                                cur = new CoordinateInfo(cur.x, cur.y + 1, 'L');
-                            } else {
-                                ans.append("R");
-                                cur = new CoordinateInfo(cur.x+1, cur.y, 'R');
-                            }
-                        } else if(cur.d == 'L') {
-                            if(cur.x == 0) {
-                                ans.append("D");
-                                cur = new CoordinateInfo(cur.x, cur.y+1,'R');
-                            } else {
-                                ans.append("L");
-                                cur = new CoordinateInfo(cur.x-1, cur.y, 'L');
-                            }
-                        }
-                    }
-                }
-            }
-
-        }
-        System.out.println(ans);
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      R = Integer.parseInt(st.nextToken());
+      C = Integer.parseInt(st.nextToken());
+      map = new int[R][C];
+      for(int i=0; i<R; i++) {
+          StringTokenizer sti = new StringTokenizer(br.readLine());
+          for(int j=0; j<C; j++) {
+              map[i][j] = Integer.parseInt(sti.nextToken());
+              if((i%2==1 && j%2==0) || (i%2==0 && j%2==1)) {
+                  if(min.x == -1) {
+                      min = new Coordinate(j,i);
+                  } else {
+                      if(map[min.y][min.x] > map[i][j]) {
+                          min = new Coordinate(j,i);
+                      }
+                  }
+              }
+          }
+      }
+      
+      if(min.y == R-1) {
+          min = new Coordinate(min.x+1, min.y);
+          last_ex = true;
+      } else {
+          min = new Coordinate(min.x+1, min.y+1);
+      }
+      
+      if(R%2==1) {
+          for(int i=1; i<=R; i++) {
+              for(int j=1; j<=C; j++) {
+                  if(i!=R || j!=C) {
+                      if(i%2==1) {
+                          if(j==C) {
+                              ans.append("D");
+                          } else {
+                              ans.append("R");
+                          }
+                      } else {
+                          if(j==C) {
+                              ans.append("D");
+                          } else {
+                              ans.append("L");
+                          }
+                      }
+                  }
+              }
+          }
+      } else if(C%2==1) {
+          for(int i=1; i<=C; i++) {
+              for(int j=1; j<=R; j++) {
+                  if(i!=C || j!=R) {
+                      if(i%2==1) {
+                          if(j==R) {
+                              ans.append("R");
+                          } else {
+                              ans.append("D");
+                          }
+                      } else {
+                          if(j==R) {
+                              ans.append("R");
+                          } else {
+                              ans.append("U");
+                          }
+                      }
+                  }
+              }
+          }
+      } else {
+          //둘다 짝수면
+          String zigzag_1,zigzag_2,c1_zigzag,c2_zigzag;
+          char zad; //zigzag after direction
+          char d = 'R';
+          boolean toggle = true;
+          if(min.y % 2 == 1) {
+              zad = 'L';
+              zigzag_1 = "DR";
+              zigzag_2 = "UR";
+              if(last_ex) {
+                c1_zigzag = "RD";
+                c2_zigzag = "RU";
+              } else {
+                c1_zigzag = "RU";
+                c2_zigzag = "RD";
+              }
+          } else {
+              min.x = C+1-min.x;
+              zad = 'R';
+              zigzag_1 = "DL";
+              zigzag_2 = "UL";
+              c1_zigzag = "LU";
+              c2_zigzag = "LD";
+          }
+          //반대 세팅
+          for(int i=1; i<=R-1; i++) {
+              for(int j=1; j<=C; j++) {
+                  if(i!=R-1 || j!=C) {
+                      if(i==min.y) {
+                          if(j==min.x) {
+                              if(j==C) {
+                                  ans.append("D");
+                                  d = zad;
+                              } else {
+                                  zigzag_1 = c1_zigzag;
+                                  zigzag_2 = c2_zigzag;
+                                  ans.append(zigzag_1);
+                                  toggle = false;
+                              }
+                          } else {
+                              if(j==C) {
+                                  ans.append("D");
+                                  d = zad;
+                              } else {
+                                  if(toggle) {
+                                      ans.append(zigzag_1);
+                                      toggle = false;
+                                  } else {
+                                      ans.append(zigzag_2);
+                                      toggle = true;
+                                  }
+                              }
+                          }
+                      } else {
+                          if(d=='R') {
+                              if(j==C) {
+                                  ans.append("D");
+                                  d='L';
+                              } else {
+                                  ans.append("R");
+                              }
+                          } else if(d=='L') {
+                              if(j==C) {
+                                  ans.append("D");
+                                  d='R';
+                              } else {
+                                  ans.append("L");
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      }
+      System.out.println(ans);
     }
 }
