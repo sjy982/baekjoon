f0f9f21f34153bbe51cd62fbdb84665a56d5762a
Sarnold <89844585+sjy982@users.noreply.github.com>
2023-01-03 23:22:53 +0900

    [Gold IV] Title: 공유기 설치, Time: 268 ms, Memory: 29228 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -17,11 +17,8 @@ public class Main {
       Arrays.sort(home);
       min_dist = 1;
       max_dist = home[home.length-1] - home[0];
-      while(min_dist != max_dist) {
-          long mid_dist;
-          //mid_dist 구하기
-          if((min_dist + max_dist)%2 == 0) mid_dist = (min_dist + max_dist)/2;
-          else mid_dist = (min_dist + max_dist)/2 + 1;
+      while(min_dist <= max_dist) {
+          long mid_dist = mid_dist = (min_dist + max_dist)/2;
           //공유기 설치
           int cout = 1;
           long last_router = home[0];
@@ -33,12 +30,12 @@ public class Main {
           }
           if(cout >= C) {
               //min_dist 업데이트
-              min_dist = mid_dist;
+              min_dist = mid_dist + 1;
           } else {
               //max_dist 업데이트
               max_dist = mid_dist - 1;
           }
       }
-      System.out.println(min_dist);
+      System.out.println(min_dist-1);
     }
 }
