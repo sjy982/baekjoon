f5c298c770e42bffcf21ce40347e79981d1f4663
Sarnold <89844585+sjy982@users.noreply.github.com>
2023-01-24 02:13:20 +0900

    [Gold III] Title: 괄호 추가하기, Time: 84 ms, Memory: 11940 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
@@ -6,7 +6,7 @@ public class Main {
     static int c_op; //연산자 개수
     static int max_pt; //최대 괄호 개수
     static Stack<Integer> result = new Stack<>();
-    static LinkedList<Character> expression = new LinkedList<>();
+    static LinkedList<String> expression = new LinkedList<>();
     static ArrayList<Long> ans = new ArrayList<>();
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,10 +15,12 @@ public class Main {
         max_pt = (N - c_op)/2;
         String str_ex = br.readLine();
         for(int i=0; i<str_ex.length(); i++) {
-            expression.add(str_ex.charAt(i));
+            expression.add(String.valueOf(str_ex.charAt(i)));
         }
         //괄호 추가하지 않았을 때 값
-        ans.add(calculate(expression));
+        LinkedList<String> copy = new LinkedList<>();
+        copy.addAll(expression);
+        ans.add(all_calculate(copy));
         //괄호 추가했을 때 모든 경우의 수
         for(int i=1; i<=max_pt; i++) {
             DFS(i,1);
@@ -27,14 +29,16 @@ public class Main {
     }
     static void DFS(int len, int s) {
         if(result.size() == len) {
-            LinkedList<Character> ex = new LinkedList<>();
+            LinkedList<String> ex = new LinkedList<>();
             ex.addAll(expression);
             for(int i=0; i<result.size(); i++) {
-                int left_ind = result.get(i) * 2 + i*2; //연산자에 왼쪽 피연산자 위치
-                ex.add(left_ind, '(');
-                ex.add(left_ind+4, ')');
+                int left_ind = result.get(i) * 2 - i*2; //연산자에 왼쪽 피연산자 위치
+                long v = calculate(Long.parseLong(ex.get(left_ind)), ex.get(left_ind+1).charAt(0), Long.parseLong(ex.get(left_ind+2)));
+                ex.set(left_ind+2 ,String.valueOf(v));
+                ex.remove(left_ind+1);
+                ex.remove(left_ind);
             }
-            ans.add(calculate(ex));
+            ans.add(all_calculate(ex));
             return;
         }
         for(int i=s; i<c_op; i++) {
@@ -44,47 +48,26 @@ public class Main {
         }
         
     }
-    static long calculate(LinkedList<Character> e) {
-        //중위 표기식 -> 후위 표기식
-        LinkedList<Character> rear_e = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
-        for(int i=0; i<e.size(); i++) {
-            if(e.get(i)=='+' || e.get(i)=='-' || e.get(i)=='*' || e.get(i)=='(') {
-                if(stack.size()==0) stack.push(e.get(i));
-                else {
-                    if(e.get(i)=='(' || stack.peek()=='(') stack.push(e.get(i));
-                    else {
-                        rear_e.add(stack.pop());
-                        stack.push(e.get(i));
-                    }
-                }
-            } else if(e.get(i)==')') {
-                rear_e.add(stack.pop());
-                stack.pop();
-            } else {
-                rear_e.add(e.get(i));
-            }
+    
+    static long all_calculate(LinkedList<String> e) {
+        while(e.size() != 1) {
+            long v = calculate(Long.parseLong(e.get(0)), e.get(1).charAt(0), Long.parseLong(e.get(2)));
+            e.set(2, String.valueOf(v));
+            e.remove(1);
+            e.remove(0);
         }
-        while(stack.size()!=0) {
-            rear_e.add(stack.pop());
+        return Long.parseLong(e.get(0));
+    }
+    
+    static long calculate(long left, char op, long right) {
+        long result = 0;
+        if(op == '+') {
+            result = left + right;
+        } else if(op == '-') {
+            result = left - right;
+        } else if(op =='*') {
+            result = left * right;
         }
-        //후위표기식 계산
-        Stack<Long> cal_stack = new Stack<>();
-        for(int i=0; i<rear_e.size(); i++) {
-            if(rear_e.get(i)=='+' || rear_e.get(i)=='-' || rear_e.get(i)=='*') {
-                long right = cal_stack.pop();
-                long left = cal_stack.pop();
-                if(rear_e.get(i)=='+') {
-                    cal_stack.push(left + right);
-                } else if(rear_e.get(i)=='-') {
-                    cal_stack.push(left - right);
-                } else if(rear_e.get(i)=='*') {
-                    cal_stack.push(left * right);
-                }
-            } else {
-                cal_stack.push((long) rear_e.get(i) - '0');
-            }
-        }
-        return cal_stack.pop();
+        return result;
     }
 }
