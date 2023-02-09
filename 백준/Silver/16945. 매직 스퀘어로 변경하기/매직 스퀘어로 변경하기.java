725cefe0102ae21ec2b4a6d7a97628f0aa9cd1bf
Sarnold <89844585+sjy982@users.noreply.github.com>
2023-02-09 23:52:21 +0900

    [Silver II] Title: 매직 스퀘어로 변경하기, Time: 844 ms, Memory: 52684 KB -BaekjoonHub

---
+++ 백준/Silver/16945. 매직 스퀘어로 변경하기/매직 스퀘어로 변경하기.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int board[][] = new int[3][3];
+    static boolean visited[] = new boolean[10];
+    static int ans = Integer.MAX_VALUE;
+    static Stack<Integer> result = new Stack<>();
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        DFS();
+        System.out.println(ans);
+    }
+    static void DFS() {
+        if(result.size()==9) {
+            if(find_mk()) {
+                int cost = 0;
+                for(int i=0; i<9; i++) {
+                    cost += Math.abs(board[i/3][i%3] - result.get(i));
+                }
+                ans = Math.min(ans, cost);
+            }
+            return;
+        }
+        for(int i=1; i<=9; i++) {
+            if(!visited[i]) {
+                result.push(i);
+                visited[i] = true;
+                DFS();
+                result.pop();
+                visited[i] = false;
+            }
+        }
+    }
+    static boolean find_mk() {
+        ArrayList<Integer> sum = new ArrayList<>();
+        //열
+        for(int i=0; i<3; i++) {
+            sum.add(result.get(i) + result.get(i+3) + result.get(i+6));
+        }
+        //행
+        for(int i=0; i<3; i++) {
+            sum.add(result.get(i*3) + result.get(i*3+1) + result.get(i*3+2));
+        }
+        //대각선
+        sum.add(result.get(0) + result.get(4) + result.get(8));
+        sum.add(result.get(2) + result.get(4) + result.get(6));
+        for(int i=0; i<7; i++) {
+            if(sum.get(i) != sum.get(i+1)) return false;
+        }
+        return true;
+    }
+}
