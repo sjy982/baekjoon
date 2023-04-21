8451c53f915eb304851837fc1e899feb5f9a2921
SJY_98 <89844585+sjy982@users.noreply.github.com>
2023-04-21 22:27:18 +0900

    [unrated] Title: 요격 시스템, Time: 0.58 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Range {
+    int s,e;
+    Range(int s, int e) {
+        this.s = s;
+        this.e = e;
+    }
+}
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        Arrays.sort(targets, (t1, t2) -> {
+            return t1[0] - t2[0];
+        });
+        Range range = new Range(0, targets[targets.length-1][1]); //최대 범위
+        for(int i=0; i<targets.length; i++) {
+            if((range.s <= targets[i][0]) && (targets[i][0] < range.e)) {
+                if(range.e <= targets[i][1]) range = new Range(targets[i][0], range.e);
+                else range = new Range(targets[i][0], targets[i][1]);
+            } else {
+                answer += 1;
+                range = new Range(targets[i][0], targets[i][1]);
+            }
+        }
+        return answer + 1;
+    }
+}
