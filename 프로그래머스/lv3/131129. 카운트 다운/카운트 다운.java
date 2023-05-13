c169baa117aa974357a4c7f5c7ce5293b5bbaba7
SJY_98 <89844585+sjy982@users.noreply.github.com>
2023-05-13 22:22:15 +0900

    [level 3] Title: 카운트 다운, Time: 0.85 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/lv3/131129. 카운트 다운/카운트 다운.java
@@ -1,42 +1,48 @@
 import java.util.*;
+class Score {
+    int score;
+    boolean sb;
+    Score(int score, boolean sb) {
+        this.score = score;
+        this.sb = sb;
+    }
+}
 class Node {
-    int cout, bs_cout;
-    Node(int cout, int bs_cout) {
-        this.cout = cout;
-        this.bs_cout = bs_cout;
+    int cnt, sb_cnt;
+    Node(int cnt, int sb_cnt) {
+        this.cnt = cnt;
+        this.sb_cnt = sb_cnt;
     }
 }
 class Solution {
+    static ArrayList<Score> score_list = new ArrayList<>();
     static Node[] dp;
     public int[] solution(int target) {
         int[] answer = new int[2];
-        dp = new Node[100001];
-        dp[50] = new Node(1, 1);
+        //다트판 모든 점수 넣기
+        score_list.add(new Score(50, true));
         for(int i=1; i<=20; i++) {
-            for(int j=1; j<=3; j++) {
-                int single = j == 1 ? 1 : 0;
-                if(dp[i * j] == null) dp[i * j] = new Node(1, single);
-                else if(dp[i * j].bs_cout < single) dp[i * j] = new Node(1, single);
-            }
+            score_list.add(new Score(i, true));
+            score_list.add(new Score(i*2, false));
+            score_list.add(new Score(i*3, false));
         }
-        for(int i=1; i<=60; i++) if(dp[i] == null) dp[i] = find_best(i, 1, i-1);
-        for(int i=61; i<=target; i++) dp[i] = find_best(i, i - 60, i - 1);
-        answer[0] = dp[target].cout;
-        answer[1] = dp[target].bs_cout;
-        return answer;
-    }
-    
-    static Node find_best(int score, int start, int end) {
-        Node n = new Node(Integer.MAX_VALUE, -1);
-        for(int i=start; i<=end; i++) {
-            if(n.cout > dp[score - i].cout + dp[i].cout) {
-                n = new Node(dp[score - i].cout + dp[i].cout, dp[score - i].bs_cout + dp[i].bs_cout);
-            } else if(n.cout == dp[score - i].cout + dp[i].cout) {
-                if(n.bs_cout < dp[score - i].bs_cout + dp[i].bs_cout) {
-                    n = new Node(n.cout, dp[score - i].bs_cout + dp[i].bs_cout);
+        dp = new Node[target + 61];
+        dp[0] = new Node(0, 0);
+        for(int i=0; i<=target; i++) {
+            for(int j=0; j<score_list.size(); j++) {
+                int next_score = i + score_list.get(j).score;
+                int next_sb_cnt = score_list.get(j).sb ? dp[i].sb_cnt + 1 : dp[i].sb_cnt;
+                if(dp[next_score] == null) dp[next_score] = new Node(dp[i].cnt + 1, next_sb_cnt);
+                else {
+                    if((dp[i].cnt + 1) < dp[next_score].cnt) dp[next_score] = new Node(dp[i].cnt + 1, next_sb_cnt);
+                    else if((dp[i].cnt + 1) == dp[next_score].cnt) {
+                        if(next_sb_cnt > dp[next_score].sb_cnt) dp[next_score].sb_cnt = next_sb_cnt;
+                    }
                 }
             }
         }
-        return n;
+        answer[0] = dp[target].cnt;
+        answer[1] = dp[target].sb_cnt;
+        return answer;
     }
 }
