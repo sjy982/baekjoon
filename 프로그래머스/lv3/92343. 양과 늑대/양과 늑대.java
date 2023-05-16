6a43947959248893516f194131814ac0e9c60dcb
SJY_98 <89844585+sjy982@users.noreply.github.com>
2023-05-16 20:42:53 +0900

    [level 3] Title: 양과 늑대, Time: 0.56 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
@@ -7,6 +7,7 @@ class Node {
     }
 }
 class Solution {
+    static boolean[] visited = new boolean[(int)Math.pow(2, 17) + 1];
     static ArrayList<Integer>[] tree;
     static int answer = -1;
     public int solution(int[] info, int[][] edges) {
@@ -15,11 +16,12 @@ class Solution {
         for(int i=0; i<edges.length; i++) tree[edges[i][0]].add(edges[i][1]);
         ArrayList<Integer> node_list = new ArrayList<>();
         node_list.add(0);
-        DFS(node_list, 0, new Node(1,0), info);
+        visited[1] = true;
+        DFS(node_list, 1, 0, new Node(1,0), info);
         return answer;
     }
     
-    static void DFS(ArrayList<Integer> node_list, int cur_n, Node info_n, int[] info) {
+    static void DFS(ArrayList<Integer> node_list, int set, int cur_n, Node info_n, int[] info) {
         for(int i=0; i<tree[cur_n].size(); i++) node_list.add(tree[cur_n].get(i)); //자식 노드 추가
         node_list.remove(Integer.valueOf(cur_n)); //현재 노드 삭제
         if((info_n.sheep == info_n.wolf) || (node_list.size() == 0)) {
@@ -29,11 +31,15 @@ class Solution {
         }
         for(int i=0; i<node_list.size(); i++) {
             int b = node_list.get(i);
-            Node next_info = new Node(info_n.sheep, info_n.wolf);
-            ArrayList<Integer> copy_node_list = deep_copy(node_list);
-            if(info[b] == 0) next_info.sheep += 1;
-            else next_info.wolf += 1;
-            DFS(copy_node_list, b, next_info, info);
+            int next_set = set | (1 << b);
+            if(!visited[next_set]) {
+                visited[next_set] = true;
+                Node next_info = new Node(info_n.sheep, info_n.wolf);
+                ArrayList<Integer> copy_node_list = deep_copy(node_list);
+                if(info[b] == 0) next_info.sheep += 1;
+                else next_info.wolf += 1;
+                DFS(copy_node_list, next_set, b, next_info, info);
+            }
         }
     }
     static ArrayList<Integer> deep_copy(ArrayList<Integer> list) {
