3a4d7cae0a380478f67ed3fd9e79ed2b8b80e740
신정용 <89844585+sjy982@users.noreply.github.com>
2024-01-10 23:08:12 +0900

    Update 장군.java

--- 백준/Gold/16509. 장군/장군.java
+++ 백준/Gold/16509. 장군/장군.java
@@ -11,13 +11,21 @@ class Node {
 }
 
 public class Main {
-    static ArrayList<Node>[] direct = new ArrayList[8];
+    static final int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static final int[] dy = {0, -1, -1, -1, 0, 1, 1, 1};
+    static final int[][] direct = {
+        {0, 7, 7}, 
+        {0, 1, 1},
+        {2, 1, 1},
+        {2, 3, 3},
+        {4, 3, 3},
+        {4, 5, 5},
+        {6, 5, 5},
+        {6, 7, 7}
+    };
     static Node king;
     static boolean[][] visited = new boolean[10][9];
     public static void main(String args[]) throws IOException {
-        for(int i=0; i<8; i++) {
-            direct[i] = new ArrayList<>();
-        }
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st1 = new StringTokenizer(br.readLine());
         int sy = Integer.parseInt(st1.nextToken());
@@ -27,7 +35,6 @@ public class Main {
         int ky = Integer.parseInt(st2.nextToken());
         int kx = Integer.parseInt(st2.nextToken());
         king = new Node(kx, ky, 0);
-        makeDirect();
         System.out.println(bfs(sang));
     }
     
@@ -41,31 +48,29 @@ public class Main {
                 return n.c;
             }
             for(int i=0; i<8; i++) {
-                Node movedNode = move(n, direct[i]);
-                if(movedNode.x != -1 && !visited[movedNode.y][movedNode.x]) {
-                    visited[movedNode.y][movedNode.x] = true;
-                    que.add(movedNode);
+                Node mv = new Node(n.x, n.y, n.c + 1);
+                boolean isPosi = true;
+                for(int j=0; j<direct[i].length; j++) {
+                    int nx = mv.x + dx[direct[i][j]];
+                    int ny = mv.y + dy[direct[i][j]];
+                    if(!checkRange(nx, ny)) {
+                        isPosi = false;
+                        break;
+                    }
+                    if(j != 2 && (nx == king.x && ny == king.y)) {
+                        isPosi = false;
+                        break;
+                    }
+                    mv = new Node(nx, ny, mv.c);
                 }
+                if(isPosi && !visited[mv.y][mv.x]) {
+                    que.add(mv);
+                    visited[mv.y][mv.x] = true;
+                }
+                
             }
         }
         return -1;
-    }
-    
-    static Node move(Node n, ArrayList<Node> route) {
-        Node result = new Node(n.x, n.y, n.c + 1);
-        for(int i=0; i<3; i++) {
-            int nx = result.x + route.get(i).x;
-            int ny = result.y + route.get(i).y;
-            if(!checkRange(nx, ny)) {
-                return new Node(-1, -1, -1);
-            }
-            if(i != 2 && (king.x == nx && king.y == ny)) {
-                return new Node(-1, -1, -1);
-            }
-            result.x = nx;
-            result.y = ny;
-        }
-        return result;
     }
     
     static boolean checkRange(int x, int y) {
@@ -73,30 +78,5 @@ public class Main {
             return true;
         }
         return false;
-    }
-    
-    static void makeDirect() {
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, -1, 0, 1};
-        int[] dgx = {-1, -1, 1, 1};
-        int[] dgy = {1, -1, -1, 1};
-        ArrayList<Integer>[] dgIndList = new ArrayList[4];
-        for(int i=0; i<4; i++) {
-            dgIndList[i] = new ArrayList<>();
-        }
-        dgIndList[0].add(0);
-        dgIndList[0].add(1);
-        dgIndList[1].add(1);
-        dgIndList[1].add(2);
-        dgIndList[2].add(2);
-        dgIndList[2].add(3);
-        dgIndList[3].add(3);
-        dgIndList[3].add(0);
-        for(int i=0; i<8; i++) {
-            direct[i].add(new Node(dx[i/2], dy[i/2], 0));
-            for(int j=0; j<2; j++) {
-                direct[i].add(new Node(dgx[dgIndList[i/2].get(i%2)], dgy[dgIndList[i/2].get(i%2)], 0));
-            }
-        }
     }
 }
