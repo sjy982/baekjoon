4c8689a152e774592a793acd8d1e83fb961301bb
신정용 <shinjy0211@gmail.com>
2024-09-29 15:45:58 +0900

    [Gold IV] Title: 미로 탈출, Time: 668 ms, Memory: 170892 KB -BaekjoonHub

--- 백준/Gold/14923. 미로 탈출/미로 탈출.java
+++ 백준/Gold/14923. 미로 탈출/미로 탈출.java
@@ -2,11 +2,12 @@ import java.io.*;
 import java.util.*;
 
 class Location {
-    int x, y, wl; //wl -> 0 안부심, wl -> 1 부심
-    Location(int x, int y, int wl) {
+    int x, y, wl, c; //wl -> 0 안부심, wl -> 1 부심
+    Location(int x, int y, int wl, int c) {
         this.x = x;
         this.y = y;
         this.wl = wl;
+        this.c = c;
     }
 }
 
@@ -24,11 +25,11 @@ public class Main {
       StringTokenizer st2 = new StringTokenizer(br.readLine());
       int hy = Integer.parseInt(st2.nextToken()) - 1;
       int hx = Integer.parseInt(st2.nextToken()) - 1;
-      Location hong = new Location(hx, hy, 0);
+      Location hong = new Location(hx, hy, 0, 1);
       StringTokenizer st3 = new StringTokenizer(br.readLine());
       int ey = Integer.parseInt(st3.nextToken()) - 1;
       int ex = Integer.parseInt(st3.nextToken()) - 1;
-      Location exit = new Location(ex, ey, 0);
+      Location exit = new Location(ex, ey, 0, 0);
       
       for(int i=0; i<N; i++) {
           StringTokenizer st4 = new StringTokenizer(br.readLine());
@@ -36,54 +37,43 @@ public class Main {
               map[i][j] = Integer.parseInt(st4.nextToken());
           }
       }
-      int[][][] wVisited = new int[2][N][M];
-      initVisited(wVisited);
-      bfs(map, hong, wVisited);
-      if(wVisited[1][exit.y][exit.x] == Integer.MAX_VALUE) {
-          System.out.println(-1);
-      } else {
-          System.out.println(Math.min(wVisited[0][exit.y][exit.x], wVisited[1][exit.y][exit.x]));
-      }
+      System.out.println(bfs(map, hong, exit));
   }
   
   
-  static void bfs(int[][] map, Location hong, int[][][] wVisited) {
+  static int bfs(int[][] map, Location hong, Location exit) {
       Queue<Location> que = new LinkedList<>();
+      boolean[][][] visited = new boolean[2][N][M];
       que.add(hong);
-      wVisited[0][hong.y][hong.x] = 1;
+      int result = Integer.MAX_VALUE;
       while(!que.isEmpty()) {
           Location node = que.poll();
+          if(node.x == exit.x && node.y == exit.y) {
+              result = Math.min(result, node.c);
+          }
           for(int i=0; i<4; i++) {
               int nx = node.x + dx[i];
               int ny = node.y + dy[i];
               if((0 <= nx && nx <= M - 1) && (0 <= ny && ny <= N - 1)) {
                   if(map[ny][nx] == 0) {
-                      if(wVisited[node.wl][node.y][node.x] + 1 < wVisited[node.wl][ny][nx]) {
-                          wVisited[node.wl][ny][nx] = wVisited[node.wl][node.y][node.x] + 1;
-                          que.add(new Location(nx, ny, node.wl));
+                      if(!visited[node.wl][ny][nx]) {
+                          visited[node.wl][ny][nx] = true;
+                          que.add(new Location(nx, ny, node.wl, node.c + 1));
                       }
                   } else {
                       //벽이라면
-                      if(node.wl == 0) {
+                      if(node.wl == 0 && !visited[1][ny][nx]) {
                           //부실 수 있음
-                          if(wVisited[node.wl][node.y][node.x] < wVisited[1][ny][nx]) {
-                              wVisited[1][ny][nx] = wVisited[node.wl][node.y][node.x];
-                              que.add(new Location(nx, ny, 1));
-                          }
+                          visited[1][ny][nx] = true;
+                          que.add(new Location(nx, ny, 1, node.c));
                       }
                   }
               }
           }
       }
-  }
-  
-  static void initVisited(int[][][] visited) {
-      for(int i=0; i<2; i++) {
-          for(int j=0; j<N; j++) {
-              for(int k=0; k<M; k++) {
-                  visited[i][j][k] = Integer.MAX_VALUE;
-              }
-          }
+      if(result == Integer.MAX_VALUE) {
+          return -1;
       }
+      return result;
   }
 }
