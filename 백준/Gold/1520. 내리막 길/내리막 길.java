0bdaf0c6f3ce667fc43cea137ebf91eabab80302
신정용 <shinjy0211@gmail.com>
2024-10-06 18:54:08 +0900

    [Gold III] Title: 내리막 길, Time: 268 ms, Memory: 39468 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,42 +1,23 @@
 import java.io.*;
 import java.util.*;
 
-class Node implements Comparable<Node> {
-    int x, y, h;
-    Node(int x, int y, int h) {
-        this.x = x;
-        this.y = y;
-        this.h = h;
-    }
-    
-    @Override
-    public int compareTo(Node o) {
-        if(this.h < o.h) {
-            return 1;
-        } else if(this.h > o.h) {
-            return -1;
-        }
-        return 0;
-    }
-}
-
 public class Main {
-    static final int dx[] = {-1, 0, 1, 0};
-    static final int dy[] = {0, -1, 0, 1};
-    static boolean[][] visited;
-    static int[][] map;
-    static int[][] dp;
+    static final int[] dx = {-1, 0, 1, 0};
+    static final int[] dy = {0, -1, 0, 1};
     static int N,M;
-  public static void main(String args[]) throws  IOException {
+    static int[][] dp;
+    static int[][] map;
+    static boolean[][] visited;
+  public static void main(String args[]) throws IOException {
+      //dfs + dp 풀이
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       StringTokenizer st = new StringTokenizer(br.readLine());
-      
       N = Integer.parseInt(st.nextToken()); //행
       M = Integer.parseInt(st.nextToken()); //열
       
-      visited = new boolean[N][M];
-      map = new int[N][M];
       dp = new int[N][M];
+      map = new int[N][M];
+      visited = new boolean[N][M];
       
       for(int i=0; i<N; i++) {
           StringTokenizer st2 = new StringTokenizer(br.readLine());
@@ -44,31 +25,34 @@ public class Main {
               map[i][j] = Integer.parseInt(st2.nextToken());
           }
       }
-      bfs();
-      System.out.println(dp[N - 1][M - 1]);
+      
+      System.out.println(dfs(0, 0));
   }
   
-  static void bfs() {
-      PriorityQueue<Node> pq = new PriorityQueue<>();
-      pq.add(new Node(0, 0, map[0][0]));
-      visited[0][0] = true;
-      dp[0][0] = 1;
-      
-      while(!pq.isEmpty()) {
-          Node n = pq.poll();
-          for(int i=0; i<4; i++) {
-              int nx = n.x + dx[i];
-              int ny = n.y + dy[i];
-              if((0<= nx && nx <= M - 1) && (0 <= ny && ny <= N-1)) {
-                  if(n.h > map[ny][nx]) {
-                      dp[ny][nx] += dp[n.y][n.x];
-                      if(!visited[ny][nx]) {
-                          visited[ny][nx] = true;
-                          pq.add(new Node(nx, ny, map[ny][nx]));
-                      }
-                  }
+  static int dfs(int x, int y) {
+      if(x == M - 1 && y == N - 1) {
+          return 1;
+      }
+      visited[y][x] = true;
+      int cnt = 0;
+      for(int i=0; i<4; i++) {
+          int nx = x + dx[i];
+          int ny = y + dy[i];
+          
+          if((0 <= nx && nx <= M - 1) && (0 <= ny && ny <= N - 1)) {
+              if(map[y][x] <=  map[ny][nx]) {
+                  //내가 가려는 곳이 같거나 더 높은 지대라면
+                  continue;
               }
+              if(visited[ny][nx]) {
+                  //내가 가려는 곳이 이미 방문했다면
+                  cnt += dp[ny][nx];
+                  continue;
+              }
+              cnt += dfs(nx, ny);
           }
       }
+      dp[y][x] = cnt;
+      return cnt;
   }
 }
