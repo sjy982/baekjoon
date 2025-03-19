464cc416eed432646ffd4c26a19008f54ffc2eb5
신정용 <shinjy0211@gmail.com>
2025-03-19 15:39:19 +0900

    [level 3] Title: n + 1 카드게임, Time: 1.21 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -1,84 +1,91 @@
-class Node {
-    int v, c;
-    Node(int v, int c) {
-        this.v = v;
-        this.c = c;
-    }
-}
+import java.io.*;
+import java.util.*;
 
 class Solution {
-    static Node[] cardsInfo;
-    static int bc;
-    static int N;
+    static int N, tSum; //target sum
     public int solution(int coin, int[] cards) {
         N = cards.length;
-        cardsInfo = new Node[N];
-        bc = N/3 - 1;
-        makeCardsInfo(cards);
-        int zCout = countZero();
-        int oCout = 0;
-        int tCout = 0;
+        tSum = N + 1;
+        HashMap<Integer, Boolean> deck = new HashMap<>(); //현재 덱
+        int pCnt = 0; //진행할 수 있는 횟수
         
-        int answer = 1;
-        //라운드 진행
-        for(int i=bc + 1; i<N; i+=2) {
-            int aInd = i;
-            int bInd = i + 1;
-            if(cardsInfo[aInd].c <= bc) {
-                oCout += 1;
-            } else if(cardsInfo[aInd].c < aInd) {
-                tCout += 1;
+        HashMap<Integer, Boolean> fDeck = new HashMap<>(); //미래 덱
+        int fPCnt = 0; //미래 진행할 수 있는 횟수.
+        
+        for(int i=0; i<N/3; i++) {
+            int pair = tSum - cards[i];
+            if(deck.get(pair) == null) {
+                //내 짝이 아직 없다면.
+                deck.put(cards[i], true);
+            } else {
+                //짝이 있다면.
+                deck.remove(pair); //삭제
+                pCnt += 1; //진행 횟수 증가.
             }
-            
-            if(cardsInfo[bInd].c <= bc) {
-                oCout += 1;
-            } else if(cardsInfo[bInd].c < bInd) {
-                tCout += 1;
-            }
-            
-            if(zCout > 0) {
-                answer += 1;
-                zCout -= 1;
-            } else if(oCout > 0 && coin > 0) {
-                answer += 1;
-                oCout -= 1;
-                coin -= 1;
-            } else if(tCout > 0 && coin >= 2) {
-                answer += 1;
-                tCout -= 1;
-                coin -= 2;
+        }
+        
+        //이제 라운드 진행.
+        int curRound = 1; //현재 라운드.
+        int cursor = N/3;
+        while(true) {
+            if(cursor < N) {
+                //cursor가 N보다 작다면
+                //두 장을 뽑는다.
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(cards[cursor]);
+                list.add(cards[cursor + 1]);
+                cursor += 2;
+                
+                for(int i=0; i<2; i++) {
+                    int num = list.get(i);
+                    int pair = tSum - num; //짝 넘버.
+                    if(deck.get(pair) != null && 1 <= coin) {
+                        //짝이 있으면서, 코인이 하나 이상 있다면.
+                        deck.remove(pair); //짝 지워주고.
+                        coin -= 1; //코인 하나 줄여주고,
+                        pCnt += 1; //진행 횟수 증가.
+                    } else if(deck.get(pair) == null) {
+                        //짝이 현재 덱에 없다면. 미래 덱에서 찾아본다.
+                        if(fDeck.get(pair) != null) {
+                            //미래 덱에 있다면.
+                            fDeck.remove(pair); //짝 지워주고.
+                            fPCnt += 1; //나중에 사용될 수 있는 진행 횟수를 증가시킨다. 이 횟수를 사용하려면 coin 2개 필요.
+                        } else {
+                            //미래 덱에 없다면. 넣어준다.
+                            fDeck.put(num, true);
+                        }
+                    }
+                }
             } else {
                 break;
             }
-        }
-        
-        return answer;
-    }
-    
-    static int countZero() {
-        int result = 0;
-        for(int i=0; i<N; i++) {
-            if(cardsInfo[i].c != -1 &&  i < cardsInfo[i].c) {
-                if(i <= bc && cardsInfo[i].c <= bc) {
-                    result += 1;
+            
+            //카드 두 개를 뽑고, 처리가 되면 이제 라운드를 진행할 수 있는 지를 체크한다.
+            if(pCnt >= 1) {
+                //진행 가능.
+                pCnt -= 1; //감소
+                curRound += 1; //다음 라운드
+            } else {
+                //pCnt가 0이라면.
+                //fPCnt로 가능한지 봐야됨.
+                if(fPCnt >= 1) {
+                    //fPCnt를 사용하기 위해서는 2개의 코인이 필요하다.
+                    if(coin >= 2) {
+                        fPCnt -= 1;
+                        coin -= 2;
+                        curRound += 1;
+                    } else {
+                        //코인이 두 개 이상 없다면. 더 이상 진행 불가.
+                        break;
+                    }
+                } else {
+                    //fPCnt도 없다면. 진행 불가.
+                    break;
                 }
             }
         }
-        return result;
-    }
-    
-    static void makeCardsInfo(int[] cards) {
-        for(int i=0; i<cards.length; i++) {
-            cardsInfo[i] = new Node(cards[i], findCombi(cards, i));
-        }
-    }
-    
-    static int findCombi(int[] cards, int ind) {
-        for(int i=0; i<cards.length; i++) {
-            if(i != ind && cards[i] + cards[ind] == N + 1) {
-                return i;
-            }
-        }
-        return -1;
+        
+        curRound = curRound > N/2 ? N/2 : curRound;
+        return curRound;
     }
 }
