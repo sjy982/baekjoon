612cf3336073d7259844d3fb10618e6f40997360
신정용 <shinjy0211@gmail.com>
2025-10-01 19:15:46 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 29.72 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,67 +1,37 @@
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        return start(cap, deliveries, pickups);
-    }
-    
-    static long start(int cap, int[] deliveries, int[] pickups) {
-        long result = 0;
-        int lastIndD = findLastInd(deliveries);
-        int lastIndP = findLastInd(pickups);
-        int lastInd = Math.max(lastIndD, lastIndP);
-          
-        while(lastInd != -1) {
-            int std = Math.max(deliveries[lastInd], pickups[lastInd]);
-            int cnt = std % cap == 0 ? std / cap : std / cap + 1;
-            result += (long) (((lastInd + 1) * 2) * cnt);
-            
-            int max = cnt * cap;
-            
-            lastIndD = update(deliveries, lastIndD, max);
-            lastIndP = update(pickups, lastIndP, max);
-            
-            lastInd = Math.max(lastIndD, lastIndP);
-        }
-        return result;
-    }
-    
-    static int update(int[] arr, int lastInd, int max) {
-        while(lastInd != -1) {
-            if(arr[lastInd] == 0) {
-                lastInd -= 1;
-                continue;
+        long answer = 0L;
+        int di = n - 1; // 배달 남은 곳의 가장 먼 인덱스
+        int pi = n - 1; // 수거 남은 곳의 가장 먼 인덱스
+
+        while (true) {
+            while (di >= 0 && deliveries[di] == 0) di--;
+            while (pi >= 0 && pickups[pi] == 0) pi--;
+            if (di < 0 && pi < 0) break; // 모두 끝
+
+            int far = Math.max(di, pi) + 1; // 1-based 거리
+            answer += 2L * far;
+
+            // 배달 처리
+            int capD = cap;
+            while (di >= 0 && capD > 0) {
+                if (deliveries[di] == 0) { di--; continue; }
+                int use = Math.min(capD, deliveries[di]);
+                deliveries[di] -= use;
+                capD -= use;
+                if (deliveries[di] == 0) di--;
             }
-            
-            if(max == 0) {
-                break;
-            }
-            
-            if(arr[lastInd] <= max) {
-                max -= arr[lastInd];
-                arr[lastInd] = 0;
-            } else {
-                arr[lastInd] -= max;
-                max = 0;
+
+            // 수거 처리
+            int capP = cap;
+            while (pi >= 0 && capP > 0) {
+                if (pickups[pi] == 0) { pi--; continue; }
+                int use = Math.min(capP, pickups[pi]);
+                pickups[pi] -= use;
+                capP -= use;
+                if (pickups[pi] == 0) pi--;
             }
         }
-        return lastInd;
-    }
-    
-    static int findLastInd(int[] arr) {
-        int lastInd = arr.length - 1;
-        boolean end = true;
-        for(int i=lastInd; i>=0; i--) {
-            if(arr[i] == 0) {
-                continue;
-            }
-            end = false;
-            lastInd = i;
-            break;
-        }
-        
-        if(end) {
-            return -1;
-        }
-        
-        return lastInd;
+        return answer;
     }
 }
