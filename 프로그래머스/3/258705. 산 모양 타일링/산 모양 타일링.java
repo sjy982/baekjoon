28e2498c39029cd7605ede6c590dd09afbbbe98c
신정용 <shinjy0211@gmail.com>
2025-10-01 18:57:41 +0900

    [level 3] Title: 산 모양 타일링, Time: 5.69 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -1,26 +1,24 @@
 class Solution {
     static final int MOD = 10007;
+
     public int solution(int n, int[] tops) {
-        int[][] dp = new int[n + 1][2]; //두 가지 타입이 있음.
-        if(tops[0] == 0) {
-            dp[1][0] = 3; //전체 모양
-            dp[1][1] = 2; //type 2
-        } else {
-            dp[1][0] = 4;
-            dp[1][1] = 3;
+        // A: 경계까지 딱 맞게 채움, B: 경계에 이어져야 완성되는 모양이 하나 남아 있음
+        int A = 1; // 아무것도 놓지 않은 상태 = 깔끔
+        int B = 1; // 왼쪽 사선 변 때문에 시작부터 '이어질 수 있는' 상태도 1가지로 본다
+
+        for (int i = 0; i < n; i++) {
+            int t = tops[i];
+            int nA, nB;
+            if (t == 0) {
+                nA = ( (2 * A) % MOD + B ) % MOD;     // A' = 2A + B
+                nB = ( A + B ) % MOD;                 // B' = A + B
+            } else {
+                nA = ( (3 * A) % MOD + B ) % MOD;     // A' = 3A + B
+                nB = ( (2 * A) % MOD + B ) % MOD;     // B' = 2A + B
+            }
+            A = nA;
+            B = nB;
         }
-        
-        for(int i=2; i<=n; i++) {
-            int top = tops[i - 1];
-            int cnt1 = 3;
-            int cnt2 = 2;
-            if(top == 0) {
-                cnt1 = 2;
-                cnt2 = 1;
-            } 
-            dp[i][0] = (cnt1 * dp[i - 1][0] + dp[i - 1][1]) % MOD;
-            dp[i][1] = (cnt2 * dp[i - 1][0] + dp[i - 1][1]) % MOD;
-        }
-        return dp[n][0];
+        return A % MOD;
     }
 }
