62cb96e1ba41aa9baad5c3c4cf2f0487a0836347
신정용 <shinjy0211@gmail.com>
2025-10-04 16:27:40 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 516.50 ms, Memory: 305 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,56 +1,67 @@
-import java.io.*;
 import java.util.*;
 
+class Info {
+    Info before;
+    char c;
+    Info(Info before, char c) {
+        this.before = before;
+        this.c = c;
+    }
+}
+
+class Info2 {
+    Info info;
+    int k, y, x;
+    Info2(Info info, int k, int y, int x) {
+        this.info = info;
+        this.k = k;
+        this.y = y;
+        this.x = x;
+    }
+}
+
 class Solution {
-    static int N, M;
-    static final int dx[] = {0, -1, 1, 0};
-    static final int dy[] = {1, 0, 0, -1};
-    static final char[] dir = {'d', 'l', 'r', 'u'};
-    static boolean[][][] visited;
-    static StringBuilder sb = new StringBuilder();
-    static String answer;
+    static final int[] dx = {0, -1, 1, 0};
+    static final int[] dy = {1, 0, 0, -1};
+    static final char[] dc = {'d', 'l', 'r', 'u'};
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        //입력에서는 x가 행이고, y가 열임. 바꿔준다. 
-        N = n;
-        M = m;
-        visited = new boolean[n][m][k + 1];
-        if(!dfs(y - 1, x - 1, 0, r - 1, c - 1, k)) {
+        Info[][][] map = new Info[k + 1][n + 1][m + 1]; //[k][행][열]
+        Queue<Info2> que = new LinkedList<>();
+        que.add(new Info2(null, 0, x, y));
+        
+        while(!que.isEmpty()) {
+            Info2 node = que.poll();
+            if (node.k == k && node.y == r&& node.x == c) {
+                break;
+            }
+            
+            if (node.k >= k) {
+                continue;
+            }
+            
+            for(int i=0; i<4; i++) {
+                int nx = node.x + dx[i];
+                int ny = node.y + dy[i];
+                if((1 <= nx && nx <= m) && (1 <= ny && ny <= n)) {
+                    if(map[node.k + 1][ny][nx] != null) {
+                        continue;
+                    }
+                    Info info = new Info(node.info, dc[i]);
+                    map[node.k + 1][ny][nx] = info;
+                    que.add(new Info2(info, node.k + 1, ny, nx));
+                }
+            }
+        }
+        if(map[k][r][c] == null) {
             return "impossible";
         }
-        return answer;
-    }
-    
-    static boolean dfs(int cx, int cy, int ck, int r, int c, int k) {
-        //여기서 x는 열, y는 행, r은 행, c는 열  
-        if(ck == k) {
-            //총 이동 횟수가 k라면.
-            if(cx == c && cy == r) {
-                //목적지라면.
-                answer = sb.toString();
-                return true;
-            }
-            return false;
+        StringBuilder sb = new StringBuilder();
+        Info cur = map[k][r][c];
+        while(cur != null) {
+            sb.append(cur.c);
+            cur = cur.before;
         }
         
-        for(int i=0; i<4; i++) {
-            int nx = cx + dx[i];
-            int ny = cy + dy[i];
-            if(checkRange(nx, ny) && !visited[ny][nx][ck + 1]) {
-                visited[ny][nx][ck + 1] = true;
-                sb.append(dir[i]);
-                if(dfs(nx, ny, ck + 1, r, c, k)) {
-                    return true;
-                }
-                sb.deleteCharAt(sb.length() - 1);
-            }
-        }
-        return false;
-    }
-    
-    static boolean checkRange(int x, int y) {
-        if((0 <= x && x <= M - 1) && (0 <= y && y <= N - 1)) {
-            return true;
-        }
-        return false;
+        return sb.reverse().toString();
     }
 }
